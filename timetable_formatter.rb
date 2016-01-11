85c6d4ca1b3aa979c87d7b42d3ba7a8d86b145f0
Artur Khantimirov <yokree@gmail.com>
2016-01-11 22:30:17 +1300

    properly handle response without result

--- timetable_formatter.rb
+++ timetable_formatter.rb
@@ -1,14 +1,19 @@
 class TimetableFormatter
   def self.format(data)
-    data["response"]["movements"].map do |bus|
+    return [] unless data['response'] && data['movements']
+    data['response']['movements'].map do |bus|
       out = {
         timestamp:                Time.parse(bus['timestamp']),
         destination:              bus['destinationDisplay'],
         stopCode:                 bus['stop_code'],
-        scheduledArrivalTime:     Time.parse(bus['scheduledArrivalTime']),
-        scheduledDepartureTime:   Time.parse(bus['scheduledArrivalTime']),
         routeShortName:           bus['route_short_name']
       }
+      if bus['scheduledArrivalTime']
+        out[:scheduledDepartureTime] = Time.parse(bus['scheduledArrivalTime'])
+      end
+      if bus['scheduledArrivalTime']
+        out[:scheduledArrivalTime] = Time.parse(bus['scheduledArrivalTime'])
+      end
       if bus['expectedArrivalTime']
         out[:expectedArrivalTime] = Time.parse(bus['expectedArrivalTime'])
       end
