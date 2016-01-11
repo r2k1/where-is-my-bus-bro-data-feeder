3d2790f473163b42a341bda16668f08811a08d8b
Artur Khantimirov <yokree@gmail.com>
2016-01-11 14:52:40 +1300

    add data formatted

--- feeder.rb
+++ feeder.rb
@@ -5,4 +5,22 @@ require 'dotenv'
 
 Dotenv.load
 Mongo::Client.new(ENV['MONGO_URL'])
-Pry::ColorPrinter.pp(AucklandTranportApi.get_data(106))
+data = AucklandTranportApi.get_data(106)
+
+def format(data)
+  data["response"]["movements"].map do |bus|
+    out = {
+      timestamp:                Time.parse(bus['timestamp']),
+      destination:              bus['destinationDisplay'],
+      stop_code:                bus['stop_code'],
+      scheduled_arrival_time:   Time.parse(bus['scheduledArrivalTime']),
+      scheduled_departure_time: Time.parse(bus['scheduledArrivalTime']),
+      route_short_name:         bus['route_short_name']
+    }
+    out[:expected_arrival_time] = Time.parse(bus['expectedArrivalTime']) if bus['expectedArrivalTime']
+    out[:expected_departure_time] = Time.parse(bus['expectedDepartureTime']) if bus['expectedDepartureTime']
+    out
+  end
+end
+
+Pry::ColorPrinter.pp(format(data))
