040c81fdff5b05a301b1e6de06129f7708888466
Artur Khantimirov <yokree@gmail.com>
2016-01-14 22:39:23 +1300

    include route name

--- at_api.rb
+++ at_api.rb
@@ -3,15 +3,22 @@ require 'uri'
 require 'json'
 
 class ATApi
-    BASE_URL = 'https://api.at.govt.nz/v1/public'
-    def self.get_locations
-      uri = build_uri('realtime/vehiclelocations')
+    BASE_URL = 'https://api.at.govt.nz/v1'
+    def get_locations
+      uri = build_uri('public/realtime/vehiclelocations')
+      json = Net::HTTP.get(uri)
+      JSON.parse(json)
+    end
+
+    def get_routes
+      uri = build_uri('gtfs/routes')
       json = Net::HTTP.get(uri)
       JSON.parse(json)
     end
 
     private
-    def self.build_uri(location)
+
+    def build_uri(location)
       URI("#{BASE_URL}/#{location}?api_key=#{ENV['AT_API_KEY']}")
     end
 end
--- vehicle_synchronizer.rb
+++ vehicle_synchronizer.rb
@@ -1,12 +1,14 @@
 require_relative 'at_api'
 require 'mongo'
 require 'dotenv'
+require 'pry'
 
 class VehicleSynchronizer
-  attr_accessor :data, :existed_data, :collection
+  attr_accessor :data, :existed_data, :collection, :routes
 
   def initialize
-    self.collection = Mongo::Client.new(ENV['MONGO_URL'])[:vehicle]
+    self.collection = Mongo::Client.new(ENV['MONGO_URL'])[:vehicles]
+    self.routes = ATApi.new.get_routes['response']
   end
 
   def sync
@@ -17,7 +19,7 @@ class VehicleSynchronizer
   private
 
   def update_data
-    self.data = ATApi.get_locations['response']['entity'].map do |e|
+    self.data = ATApi.new.get_locations['response']['entity'].map do |e|
       {
         route_id: e['vehicle']['trip']['route_id'],
         lat: e['vehicle']['position']['latitude'],
@@ -29,11 +31,15 @@ class VehicleSynchronizer
   end
 
   def create_query
-    vehicles_to_create.map { |v| { insert_one: v } }
+    vehicles = vehicles_to_create
+    p "Create #{vehicles.count}"
+    vehicles.map { |v| { insert_one: v } }
   end
 
   def update_query
-    vehicles_to_update.map do |v|
+    vehicles = vehicles_to_update
+    p "Update #{vehicles.count}"
+    vehicles.map do |v|
       {
         update_one: {
           filter: { _id: v[:_id] },
@@ -44,7 +50,9 @@ class VehicleSynchronizer
   end
 
   def remove_query
-    vehicles_to_remove.map do |v|
+    vehicles = vehicles_to_remove
+    p "Remove #{vehicles.count}"
+    vehicles.map do |v|
       {
         delete_one: {
           filter: { _id: v[:_id] }
@@ -54,9 +62,15 @@ class VehicleSynchronizer
   end
 
   def vehicles_to_create
-    data.select do |row|
+    vehicles = data.select do |row|
       !existed_data.any? { |e| e[:_id] == row[:_id] }
     end
+    vehicles.each do |vehicle|
+      route = routes.find { |r| r['route_id'] = vehicle[:route_id] }
+      vehicle[:route_short_name] = route['route_short_name']
+      vehicle[:route_long_name] = route['route_long_name']
+    end
+    vehicles
   end
 
   def vehicles_to_remove
